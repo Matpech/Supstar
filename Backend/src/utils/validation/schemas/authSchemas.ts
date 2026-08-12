d52e2a16d90f3939be301455b94cfe1afa52b1b2
Matpech <315011607+Matpech@users.noreply.github.com>
2026-08-12 21:40:58 +0200

    Invalidated '-d' suffix on the registration schema to prevent issues with the Discord SSO

--- Backend/src/utils/validation/schemas/authSchemas.ts
+++ Backend/src/utils/validation/schemas/authSchemas.ts
@@ -2,7 +2,9 @@ import Joi from "joi";
 
 export const registrationSchema = Joi.object({
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    username: Joi.string().min(3).max(32).required(),
+    username: Joi.string().min(3).max(32).custom((val, helpers) => {
+        if (val.endsWith("-d")) return helpers.error("suffix.discord")
+    }).messages({ "suffix.discord": "Suffix '-d' is not allowed" }).required(),
     password: Joi.string().min(8).max(128).required()
 })
 
