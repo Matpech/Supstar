864bdaa43740f29e732e71a459b3d02a8dac4de6
Matpech <315011607+Matpech@users.noreply.github.com>
2026-08-12 14:03:54 +0200

    Added database + Docker

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,5 +1,6 @@
 import Express from "express"
 import bodyParser from "body-parser"
+import { pool } from "./src/utils/db"
 
 const PORT = 4000
 const app = Express()
@@ -7,8 +8,20 @@ const app = Express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get("/test", (req, res) => {
-    res.send("API is running")
+app.get("/test", async (req, res) => {
+    try {
+        pool.query("SELECT 1")
+        return res.json({
+            api: "OK",
+            database: "OK"
+        })
+    } catch (error) {
+        return res.json({
+            api: "OK",
+            database: "ERROR",
+            err: error
+        })
+    }
 })
 
 app.listen(PORT, () => {
