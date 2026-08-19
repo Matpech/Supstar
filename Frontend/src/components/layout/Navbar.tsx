136873812d928f12240d9524c8d4e560df479635
Matpech <315011607+Matpech@users.noreply.github.com>
2026-08-19 13:42:42 +0200

    Added responsiveness to Navbar component

--- Frontend/src/components/layout/Navbar.tsx
+++ Frontend/src/components/layout/Navbar.tsx
@@ -1,35 +1,36 @@
 import { MapPin } from "lucide-react"
 import { useAuth } from "../../hooks/useAuth"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function Navbar() {
     const auth = useAuth()
+    const navigate = useNavigate()
 
     return (
         <div
             className="
                 flex justify-between
-                m-4 py-2 px-16
+                mx-2 md:mx-4 my-4 py-2 px-2 md:px-16
                 border-2 rounded-2xl
             "
         >
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 hover:cursor-pointer" onClick={() => navigate("/")}>
                 <MapPin size={40} />
                 <h1
-                    className="text-4xl font-bold"
+                    className="text-2xl md:text-4xl font-bold"
                 >
                     Supstar
                 </h1>
             </div>
 
             <nav className="flex items-center gap-8">
-                <Link to="/settings" className="text-2xl">
+                <Link to="/settings" className="md:text-2xl">
                     Settings
                 </Link>
 
                 <button
                     onClick={auth.logout}
-                    className="text-2xl hover:cursor-pointer"
+                    className="md:text-2xl hover:cursor-pointer"
                 >
                     Log out
                 </button>
